675c7f7d5caf79097267210ba32b6efd826516a4
LucaPalumbo <luca183.palumbo@gmail.com>
2026-05-07 15:08:19 +0200

    fix typo in yaml parameter

--- src/track/trackLoader.cpp
+++ src/track/trackLoader.cpp
@@ -84,9 +84,9 @@ Track loadMap(string mapPath, Eigen::Vector3d& start_position, Eigen::Vector3d& 
         centerline_raw = track["centerline_raw"];
     }
     Node centerline_smoothed;
-    if (track["centerline_smoot"])
+    if (track["centerline_smooth"])
     {
-        centerline_smoothed = track["centerline_smoot"];
+        centerline_smoothed = track["centerline_smooth"];
     }
     // TODO: Catch pos or orientation size != 3
     std::vector<double> start_pos = track["start"]["position"].as<vector<double>>();
